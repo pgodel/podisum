dd4447ea4e919df9f093d93accf92c412512f249
Pablo Godel <pgodel@gmail.com>
2013-05-07 17:40:58 -0400

    implemented basic angularjs app

---
+++ web/js/app.js
@@ -0,0 +1,54 @@
+var podisumApp = angular.module('podisumApp',
+                [
+                    'collectionServices',
+                    'ngResource',
+                    'ui.bootstrap'
+                ]).config(function ($routeProvider, $httpProvider, $interpolateProvider) {
+
+//            $interpolateProvider.startSymbol('[[').endSymbol(']]')
+
+            $routeProvider.
+                    when("/list", {controller: 'ListCollectionsCtrl', templateUrl: "templates/list_controllers.html"}).
+                    otherwise({redirectTo: "/list"});
+
+        });
+
+/* controllers */
+
+
+podisumApp.controller('ListCollectionsCtrl', ['$scope', 'Collection', '$timeout', function ($scope, Collection, $timeout) {
+    $scope.loading = true;
+    $scope.collections = [];
+
+    $scope.loadCollections = function() {
+        $scope.loading = true;
+        $scope.collections = Collection.query(function() {
+            $scope.loading = false;
+        });
+
+    }
+
+    $scope.getEntryClass = function(counter, avg) {
+        return counter > avg ? 'badge-important' : 'badge-info';
+    }
+
+    $scope.autoReload = function() {
+        $scope.loadCollections();
+        return;
+        $timeout( function() {
+            $scope.autoReload();
+        }, 10000);
+
+    }
+
+    $scope.autoReload();
+
+}]);
+
+
+angular.module('collectionServices', ['ngResource']).
+        factory('Collection', function ($resource) {
+            return $resource('/api/collections/:collectionId', {}, {
+                query: {method: 'GET', params: {collectionId: ''}, isArray: true}
+            });
+        });
