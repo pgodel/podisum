fc2722d56bb78b41b2fb93816a216d5b62912921
Pablo Godel <pgodel@gmail.com>
2013-05-07 10:22:28 -0400

    added podisum_cfg service

--- src/app.php
+++ src/app.php
@@ -2,21 +2,31 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tobiassjosten\Silex\ResponsibleServiceProvider;
 
 $mongoClass = class_exists('MongoClient') ? 'MongoClient' : 'Mongo';
+
+$app['podisum_cfg'] = $app->share(function()
+{
+    return \Symfony\Component\Yaml\Yaml::parse('../config/podisum.yml');
+});
+
 $app->register(new Sfk\Silex\Provider\MongoDBServiceProvider(), array(
-    'mongodb.server' => 'mongodb://localhost:27017/summarizer',
+    'mongodb.server' => $app['podisum_cfg']['mongo'],
     'mongodb.options' => array(),
     'mongodb.client_class' => $mongoClass,
 ));
+
+$app->register(new ResponsibleServiceProvider());
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../resources/views',
 ));
 
+
 $app['podisum'] = function($app)
 {
-    return new \Podisum($app['mongodb.client'], \Symfony\Component\Yaml\Yaml::parse('../config/podisum.yml'));
+    return new \Podisum($app['mongodb.client'], $app['podisum_cfg']);
 };
 
 include 'app-api.php';
