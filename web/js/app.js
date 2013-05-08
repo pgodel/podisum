2b4ab5c30400b48ac5ce6afcfeb0c06bbcf8a9f0
Pablo Godel <pgodel@gmail.com>
2013-05-07 23:16:43 -0400

    new UI features: up/down; fixed sorting

--- web/js/app.js
+++ web/js/app.js
@@ -20,6 +20,35 @@ podisumApp.controller('ListCollectionsCtrl', ['$scope', 'Collection', '$timeout'
     $scope.loading = true;
     $scope.collections = [];
 
+    $scope.getCollectionByName = function(name) {
+        if ($scope.collections.length == 0) {
+            return null;
+        }
+        for (var index in $scope.collections) {
+            if ($scope.collections[index].name == name) {
+                return $scope.collections[index];
+            }
+        }
+        return null;
+    }
+
+    $scope.getSummaryByName = function(summaries, name) {
+        for (var index in summaries) {
+            if (index == name) {
+                return summaries[index];
+            }
+        }
+        return null;
+    }
+
+    $scope.getEntryByField = function(entries, val) {
+        for (var index in entries) {
+            if (entries[index].field == val) {
+                return entries[index];
+            }
+        }
+        return null;
+    }
 
     var data;
 
@@ -27,7 +56,62 @@ podisumApp.controller('ListCollectionsCtrl', ['$scope', 'Collection', '$timeout'
         $scope.loading = true;
         data = Collection.query(function() {
             $scope.loading = false;
-            $scope.collections = data;
+
+            if ($scope.collections.length == 0) {
+                $scope.collections = data;
+                return;
+            }
+
+            angular.forEach(data, function(collection) {
+                if (!collection) {
+                    return;
+                }
+                var coll = $scope.getCollectionByName(collection.name);
+                if (null === coll) {
+                    $scope.collections.push(coll);
+                    return;
+                }
+                angular.forEach(collection.summaries, function(summary, index) {
+                    var summ = $scope.getSummaryByName(coll.summaries, index);
+                    if (null === summ) {
+                        coll.summaries.push(summ);
+                        return;
+                    }
+
+                    summ.totalUp = summ.total < summary.total;
+                    summ.totalDown = summ.total > summary.total;
+                    summ.total = summary.total;
+                    summ.avgUp = summ.avg < summary.avg;
+                    summ.avgDown = summ.avg > summary.avg;
+                    summ.avg = summary.avg;
+                    summ.avgmUp = summ.avgm < summary.avgm;
+                    summ.avgmDown = summ.avgm > summary.avgm;
+                    summ.avgm = summary.avgm;
+
+                    angular.forEach(summary.entries, function(entry) {
+                        var e = $scope.getEntryByField(summ.entries, entry.field);
+                        if (null === e) {
+                            entry.up = true;
+                            summ.entries.push(entry);
+                            return;
+                        }
+                        if (e.counter < entry.counter) {
+                            e.counter = parseInt(entry.counter);
+                            e.up = true;
+                        } else {
+                            e.up = false;
+                        }
+                    });
+
+                    // remove non-existing entries
+                    angular.forEach(summ.entries, function(entry, index) {
+                        var e = $scope.getEntryByField(summary.entries, entry.field);
+                        if (null === e) {
+                            summ.entries.splice(index,1);
+                        }
+                    });
+                });
+            });
         });
 
     }
